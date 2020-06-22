f9ee07d79cd435d5127364a8d5d0918fb5208226
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-21 22:45:15 -0400

    Box shadow for nav and other minor touchups

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,19 @@
-// Scrolling Navbar
+//Nav bar and Corner arrow button
 var navbar = document.getElementById("navbar");
-
-window.onscroll = function() {scrollFunction()};
-function scrollFunction(){
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
-        navbar.style.padding = "15px 72px";
-    }
-    else{
-        navbar.style.padding = "25px 72px";
-    }
-}
-
-//Read more / To top button
+var navLinks = document.querySelector(".nav-links > ul");
 var topBtn = document.getElementById("topBtn");
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        navbar.style.padding = "10px 72px";
+        navLinks.style.fontSize = "16px";
         topBtn.classList.add("down");
         topBtn.parentElement.href = "#home";
     }
     else{
+        navbar.style.padding = "25px 72px";
+        navLinks.style.fontSize = "18px";
         topBtn.classList.remove("down");
         topBtn.parentElement.href = "#about";
     }
