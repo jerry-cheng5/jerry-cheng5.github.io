7920de05e503749349596ed0888b8ce988016a6f
jerry-cheng5 <j.cheng55555@gmail.com>
2020-06-24 00:42:47 -0400

    Trying to get json to work

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,6 @@ const navLinks = document.querySelector(".nav-links > ul");
 const topBtn = document.getElementById("topBtn");
 const experience = document.getElementById("experiences");
 const timeline = document.querySelector("#experiences > .vertical-line")
-const coursesBtn = document.querySelector(".education > div > button");
 
 //Scroll functions
 window.onscroll = function() {
@@ -33,7 +32,7 @@ experience.style.height = "200vh";
 timeline.style.height = experience.style.height;
 
 //Experiences
-var jsonData;
+/* var jsonData;
 const experiences = document.querySelector("#experiences > .container");
 fetch("assets/js/variables.json")
     .then(function(resp){
@@ -43,6 +42,59 @@ fetch("assets/js/variables.json")
         jsonData = data;
         for(i in jsonData.experiences){
             if (jsonData.experiences[i].education == true){
+                //Making the courses popout
+                var popout = document.createElement("div");
+                popout.classList.add("popout");
+                popout.id = jsonData.experiences[i].id + "-popout";
+                
+                var header = document.createElement("div");
+                header.classList.add("courses-header", "row");
+
+                if (jsonData.experiences[i].summer != null){
+                    var leftBtn = document.createElement("div");
+                    leftBtn.classList.add("courses-left", "d-inline", "col-2");
+                    var chevronLeft = document.createElement("i");
+                    chevronLeft.classList.add("fas", "fa-chevron-left");
+                    leftBtn.appendChild(chevronLeft);
+                    var title = document.createElement("h1");
+                    title.classList.add("courses-title", "d-inline", "col");
+                    title.innerHTML = "Courses";
+                    var rightBtn = document.createElement("div");
+                    rightBtn.classList.add("courses-right", "d-inline", "col-2");
+                    var chevronRight = document.createElement("i");
+                    chevronRight.classList.add("fas", "fa-chevron-right");
+                    rightBtn.appendChild(chevronRight);
+                    header.appendChild(leftBtn);
+                    header.appendChild(title);
+                    header.appendChild(rightBtn);
+                }
+                else{
+                    var title = document.createElement("h1");
+                    title.classList.add("courses-title", "d-inline", "col");
+                    title.innerHTML = "Courses";
+                    header.appendChild(title);
+                }
+
+                var courses = document.createElement("div");
+                courses.classList.add("courses");
+                var average = document.createElement("p");
+                average.classList.add("average");
+                average.innerHTML = jsonData.experiences[i].average;
+                var list = document.createElement("ul");
+                for (j in jsonData.experiences[i].courses){
+                    var listItem = document.createElement("li");
+                    var course = document.createElement("p");
+                    course.innerHTML = jsonData.experiences[i].courses[j];
+                    listItem.appendChild(course);
+                    list.appendChild(listItem);
+                }
+                courses.appendChild(average);
+                courses.appendChild(list);
+
+                popout.appendChild(header);
+                popout.appendChild(courses);
+                experiences.appendChild(popout);
+
                 //Making the Education card
                 var node = document.createElement("div");
                 node.classList.add("education", "row");
@@ -86,76 +138,156 @@ fetch("assets/js/variables.json")
                 node.appendChild(timeWrapper);
                 experiences.appendChild(node);
 
-                //Making the courses popout
+                var query = ".education > div > #" + jsonData.experiences[i].id;
+                var secondQuery = jsonData.experiences[i].id + "-popout";
+                const coursesBtn = document.querySelector(query);
+                coursesBtn.onclick = function(){
+                    document.getElementById(secondQuery).classList.toggle("active");
+                    if (document.getElementById(secondQuery).classList.contains("active")){
+                        coursesBtn.innerHTML = "Hide Courses";
+                    }
+                    else{
+                        coursesBtn.innerHTML = "Show Courses";
+                    }   
+                };
+                
+            }
+            else{
 
             }
         }
-    });
-
-/* 
-
-<div id="popout">
-    <div class="courses-header row">
-        <div class="courses-left d-inline col-2"><i class="fas fa-chevron-left"></i></div>
-        <h1 class="courses-title d-inline col">Courses</h1>
-        <div class="courses-right d-inline col-2"><i class="fas fa-chevron-right"></i></div>
-    </div>
-    <div class="courses">
-        <p class="average">Average: 91%<p>
-        <ul>
-            <li><p>English</p></li>
-            <li><p>Math</p></li>
-            <li><p>Science</p></li>
-            <li><p>French</p></li>
-            <li><p>Geography</p></li>
-            <li><p>Health & PE</p></li>
-            <li><p>Music</p></li>
-            <li><p>Information Technology in a Global Society (IB1)</p></li>
-            <li><p>Introduction to Computer Science</p></li>
-        </ul>
-    </div>
-</div> 
-
-*/
+    }); */
 
 
+    let jsonData;
+          $.ajax({
+            url: "assets/js/variables.json",
+            dataType: "json",
+            cache: false,
+            success: function (data) {
+              jsonData = data;
+            }.bind(this),
+            error: function (xhr, status, err) {
+              console.log(err);
+              alert(err);
+            },
+            async: false,
+          });
 
-        /* var node = document.createElement("div");
-        node.id = "popout";
 
+for(i in jsonData.experiences){
+    if (jsonData.experiences[i].education == true){
+        //Making the courses popout
+        var popout = document.createElement("div");
+        popout.classList.add("popout");
+        popout.id = jsonData.experiences[i].id + "-popout";
+        
         var header = document.createElement("div");
-        header.classList.add("courses-header row");
+        header.classList.add("courses-header", "row");
 
-        var leftBtn = document.createElement("div");
-        leftBtn.classList.add("courses-left d-inline col-2");
-        var chevronLeft = document.createElement("i");
-        chevronLeft.classList.add("fas fa-chevron-left");
-        leftBtn.appendChild(chevronLeft);
+        if (jsonData.experiences[i].summer != null){
+            var leftBtn = document.createElement("div");
+            leftBtn.classList.add("courses-left", "d-inline", "col-2");
+            var chevronLeft = document.createElement("i");
+            chevronLeft.classList.add("fas", "fa-chevron-left");
+            leftBtn.appendChild(chevronLeft);
+            var title = document.createElement("h1");
+            title.classList.add("courses-title", "d-inline", "col");
+            title.innerHTML = "Courses";
+            var rightBtn = document.createElement("div");
+            rightBtn.classList.add("courses-right", "d-inline", "col-2");
+            var chevronRight = document.createElement("i");
+            chevronRight.classList.add("fas", "fa-chevron-right");
+            rightBtn.appendChild(chevronRight);
+            header.appendChild(leftBtn);
+            header.appendChild(title);
+            header.appendChild(rightBtn);
+        }
+        else{
+            var title = document.createElement("h1");
+            title.classList.add("courses-title", "d-inline", "col");
+            title.innerHTML = "Courses";
+            header.appendChild(title);
+        }
 
-        var title = document.createElement("h1");
-        title.classList.add("courses-title d-inline col");
-        title.innerHTML = "Courses";
+        var courses = document.createElement("div");
+        courses.classList.add("courses");
+        var average = document.createElement("p");
+        average.classList.add("average");
+        average.innerHTML = jsonData.experiences[i].average;
+        var list = document.createElement("ul");
+        for (j in jsonData.experiences[i].courses){
+            var listItem = document.createElement("li");
+            var course = document.createElement("p");
+            course.innerHTML = jsonData.experiences[i].courses[j];
+            listItem.appendChild(course);
+            list.appendChild(listItem);
+        }
+        courses.appendChild(average);
+        courses.appendChild(list);
 
-        var rightBtn = document.createElement("div");
-        rightBtn.classList.add("courses-right d-inline col-2");
-        var chevronRight = document.createElement("i");
-        chevronRight.classList.add("fas fa-chevron-right");
-        rightBtn.appendChild(chevronRight);
+        popout.appendChild(header);
+        popout.appendChild(courses);
+        experiences.appendChild(popout);
 
-        header.appendChild(leftBtn);
-        header.appendChild(title);
-        header.appendChild(rightBtn); */
+        //Making the Education card
+        var node = document.createElement("div");
+        node.classList.add("education", "row");
 
+        var description = document.createElement("div");
+        description.classList.add("d-inline", "col", "description");
 
+        var organization = document.createElement("h1");
+        organization.classList.add("d-block");
+        organization.innerHTML = jsonData.experiences[i].organization;
+        var location = document.createElement("h4");
+        location.classList.add("d-block");
+        location.innerHTML = jsonData.experiences[i].location;
+        var name = document.createElement("h5");
+        name.classList.add("d-block");
+        name.innerHTML = jsonData.experiences[i].name;
+        var button = document.createElement("button");
+        button.type = "button";
+        button.innerHTML = "Show Courses";
+        button.id = jsonData.experiences[i].id;
+        description.appendChild(organization);
+        description.appendChild(location);
+        description.appendChild(name);
+        description.appendChild(button);
 
-//Courses
-coursesBtn.onclick = function(){
-    document.getElementById("popout").classList.toggle("active");
-    if (document.getElementById("popout").classList.contains("active")){
-        coursesBtn.innerHTML = "Hide Courses";
+        var emblem = document.createElement("div");
+        emblem.classList.add("d-inline", "col-1", "ed-emblem");
+        var icon = document.createElement("i");
+        icon.classList.add("fas", "fa-graduation-cap");
+        emblem.appendChild(icon);
+
+        var timeWrapper = document.createElement("div");
+        timeWrapper.classList.add("d-inline", "col");
+        var time = document.createElement("h5");
+        time.style.textAlign = "left";
+        time.innerHTML = jsonData.experiences[i].time;
+        timeWrapper.appendChild(time);
+        
+        node.appendChild(description);
+        node.appendChild(emblem);
+        node.appendChild(timeWrapper);
+        experiences.appendChild(node);
+
+        var query = ".education > div > #" + jsonData.experiences[i].id;
+        var secondQuery = jsonData.experiences[i].id + "-popout";
+        const coursesBtn = document.querySelector(query);
+        coursesBtn.onclick = function(){
+            document.getElementById(secondQuery).classList.toggle("active");
+            if (document.getElementById(secondQuery).classList.contains("active")){
+                coursesBtn.innerHTML = "Hide Courses";
+            }
+            else{
+                coursesBtn.innerHTML = "Show Courses";
+            }   
+        };
+        
     }
     else{
-        coursesBtn.innerHTML = "Show Courses";
+
     }
-        
-};
+}
